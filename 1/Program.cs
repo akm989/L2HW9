0db5794dead44f187a7a83ebc86ed82e4e51c045
Akmal <akmal_179a@mail.ru>
2022-10-06 22:17:50 +0500

    Solved

---
+++ 1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все чётные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2"
+
+
+
+Console.Beep();
+Console.Clear();
+Console.WriteLine("Введите число N");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+string Numbers(int n)
+{
+
+if ( n % 2 != 0)
+{
+    n = n - 1;
+}
+    if (n == 0)
+    {
+        return " ";
+    }
+    else
+    {
+        if (n % 2 == 0)
+        {
+            return Numbers(n - 1) + n;
+        }
+        else 
+        {
+            return " ";
+        }
+    }
+
+}
+Console.Write(Numbers(a));
